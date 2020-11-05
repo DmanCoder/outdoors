e3d93fbeec40817fb6816a9fd09ccaee8f636105
DmanCoder <ktdenis.1@live.com>
2020-11-05 12:20:09 +1000

    Fixed file errors

---
+++ src/components/app.js
@@ -0,0 +1,18 @@
+// Styles
+import '../styles/main.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h2>TESTING 2134</h2>
+      <h2>TESTING 2134</h2>
+      <h2>TESTING 2134</h2>
+      <h2>TESTING 2134</h2>
+      <h2>TESTING 2134</h2>
+      <h2>TESTING 2134</h2>
+      <h2>TESTING 2134</h2>
+    </div>
+  );
+};
+
+export default App;
