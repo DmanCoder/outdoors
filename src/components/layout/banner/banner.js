c118fcfdee77dd2ae2a18800962b4c9585da9fd4
DmanCoder <ktdenis.1@live.com>
2020-11-06 14:23:00 +1000

    Styled side navigation

--- src/components/layout/banner/banner.js
+++ src/components/layout/banner/banner.js
@@ -1,19 +1,10 @@
 import React from 'react';
 
 import bannerVideo from '../../../assets/videos/mountain-clouds.mp4';
-// import bannerThumbnail from '../../../assets/imgs/thumbnail.jpg';
 
 const Banner = () => {
   return (
     <div className="banner">
-      {/* <video playsinline autoplay loop muted> */}
-
-      {/* <div className="video-size">
-        <video autoPlay loop muted>
-          <source src={bannerVideo} type="video/mp4" />
-        </video>
-      </div> */}
-
       <div id="wrapper-video">
         <video autoPlay loop muted>
           <source
--- src/components/layout/nav/navSide.js
+++ src/components/layout/nav/navSide.js
@@ -1,7 +1,26 @@
 import React from 'react';
 
+// SVS
+import { ReactComponent as Facebook } from '../../../assets/icons/facebook.svg';
+import { ReactComponent as Ig } from '../../../assets/icons/ig.svg';
+import { ReactComponent as Twitter } from '../../../assets/icons/twitter.svg';
+
 const NavSide = () => {
-  return <div className="side-nav"></div>;
+  return (
+    <div className="side-nav">
+      <ul className="side-nav__icons">
+        <li>
+          <Facebook />
+        </li>
+        <li>
+          <Twitter />
+        </li>
+        <li>
+          <Ig />
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default NavSide;
